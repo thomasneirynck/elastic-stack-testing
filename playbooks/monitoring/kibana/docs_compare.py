85bb6d9dcfcb267cb1afd509b09dcf9c628d91ff
Chris Roberson <chrisronline@gmail.com>
2020-07-23 15:51:06 -0400

    special case for temp telemetry issue (#634)

--- playbooks/monitoring/kibana/docs_compare.py
+++ playbooks/monitoring/kibana/docs_compare.py
@@ -4,6 +4,7 @@ Usage:
 '''
 
 from docs_compare_util import check_parity
+import sys
 
 allowed_deletions_from_metricbeat_docs_extra = [
   # 'path.to.field'
@@ -20,9 +21,16 @@ def handle_special_case_kibana_settings(legacy_doc, metricbeat_doc):
     and legacy_doc["kibana_settings"]["xpack"]["default_admin_email"] == None:
     legacy_doc["kibana_settings"]["xpack"].pop("default_admin_email")
 
+def handle_special_case_kibana_stats(legacy_doc, metricbeat_doc):
+  # Special case this until we have resolution on https://github.com/elastic/kibana/pull/70677#issuecomment-662531529
+  metricbeat_doc["kibana_stats"]["usage"]["search"]["total"] = legacy_doc["kibana_stats"]["usage"]["search"]["total"]
+  metricbeat_doc["kibana_stats"]["usage"]["search"]["averageDuration"] = legacy_doc["kibana_stats"]["usage"]["search"]["averageDuration"]
+
 def handle_special_cases(doc_type, legacy_doc, metricbeat_doc):
     if doc_type == "kibana_settings":
         handle_special_case_kibana_settings(legacy_doc, metricbeat_doc)
+    if doc_type == "kibana_stats":
+        handle_special_case_kibana_stats(legacy_doc, metricbeat_doc)
 
 
 check_parity(handle_special_cases, allowed_deletions_from_metricbeat_docs_extra=allowed_deletions_from_metricbeat_docs_extra)
