f170b1e77e3a51436f84cb87d3c106260b9afb17
liza-mae <liza-mae@users.noreply.github.com>
2021-01-07 09:52:55 -0700

    Another fix  (#749)
    * Missed one
    
    * Fix again

--- ci/upgrade/buildSrc/src/main/java/org/estf/gradle/CreateEssDeployment.java
+++ ci/upgrade/buildSrc/src/main/java/org/estf/gradle/CreateEssDeployment.java
@@ -188,8 +188,8 @@ public class CreateEssDeployment extends DefaultTask {
         TopologySize topologySize = getTopologySize();
 
         ElasticsearchNodeType esNodeType = new ElasticsearchNodeType().data(true).master(true).ingest(false).ml(false);
-        ElasticsearchNodeType ingestNodeType = new ElasticsearchNodeType().data(false).master(false).ingest(true);
-        ElasticsearchNodeType mlNodeType = new ElasticsearchNodeType().data(false).master(false).ml(true);
+        ElasticsearchNodeType ingestNodeType = new ElasticsearchNodeType().data(false).master(false).ml(false).ingest(true);
+        ElasticsearchNodeType mlNodeType = new ElasticsearchNodeType().data(false).master(false).ingest(false).ml(true);
 
         ElasticsearchClusterTopologyElement esTopology = new ElasticsearchClusterTopologyElement()
                 .instanceConfigurationId(esInstanceCfg)
