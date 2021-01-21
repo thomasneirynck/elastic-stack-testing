5b50a56fe159ba7aa79ae8fb587635a0570da972
liza-mae <liza-mae@users.noreply.github.com>
2021-01-21 10:10:14 -0700

    Remove oss testing (#770)

--- playbooks/stack_testing/ci/jenkins_stack_testing.sh
+++ playbooks/stack_testing/ci/jenkins_stack_testing.sh
@@ -2,16 +2,9 @@
 #
 # @author: Liza Dayoub
 
-if [ $CI_BUILD == "true" ]; then
-    export ES_BUILD_OSS=$CI_OSS
-    if [ $ES_BUILD_OSS == "true" ]; then
-      export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/stack_testing/install_no_xpack.yml"
-    else
-      export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/stack_testing/install_xpack.yml"
-    fi
-    # This will eventually be added to the CI_ stuff
-    export ES_BUILD_PKG_EXT=tar
-    export AIT_VM=vagrant_vm
-fi
+
+export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/stack_testing/install_xpack.yml"
+export ES_BUILD_PKG_EXT=tar
+export AIT_VM=vagrant_vm
 
 source jenkins_build.sh
