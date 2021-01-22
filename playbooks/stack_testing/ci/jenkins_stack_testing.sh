74af9b010c8430bc16cc97bc866883a71245f30b
liza-mae <liza-mae@users.noreply.github.com>
2021-01-22 14:22:00 -0700

    Fix build type (#776) (#779)

--- playbooks/stack_testing/ci/jenkins_stack_testing.sh
+++ playbooks/stack_testing/ci/jenkins_stack_testing.sh
@@ -6,5 +6,6 @@
 export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/stack_testing/install_xpack.yml"
 export ES_BUILD_PKG_EXT=tar
 export AIT_VM=vagrant_vm
+export ES_BUILD_OSS=false
 
 source jenkins_build.sh
